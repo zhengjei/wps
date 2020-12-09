8f3f6e007b2aeb18bf759128d9eb9a70e9cbaad9
zj <2501178392@qq.com>
2020-12-09 15:52:03 +0800

    wps加载项开发

---
+++ js/apiJson.js
@@ -0,0 +1,39 @@
+function apiJson() {
+   result="";
+    $.ajax({
+        async:false,
+        type: "GET",
+        url: "https://api.apishop.net/common/joke/getJokesByRandom?apiKey=Xn8TqB9a2294f6a36e4716726bce47baf9df0815204776e&pageSize=20",
+        dataType: "json",
+        success: function(data) {
+            var list = "";
+            result = data.result;
+            var keys = Object.keys(result[0])
+            for (var i = 0; i < keys.length; i++) {
+                var key = keys[i]
+                list +=
+                    '<li><button style="margin:3px;" class="b1" value="'+key+'">'+key+'</button></li>'
+                    // '<li><button style="margin:3px;" id="b1" onclick="f('+key+')">'+key+'</button></li>'
+            }
+                document.getElementById('ol').innerHTML = list;
+        }
+    });
+   
+}
+function f(key) {
+    console.log(key)
+    var arr = Object.keys(result);
+    let curSheet = wps.EtApplication().ActiveSheet;
+    if (curSheet){
+        var i=1;
+        while (curSheet.Cells.Item(1, i).Formula){
+            i++;
+        }
+            curSheet.Cells.Item(1, i).Formula = key
+            for (let j = 1; j <= arr.length; j++) {
+                curSheet.Cells.Item(1 + j, i).Formula = result[j - 1][key]
+            }
+            // curSheet.Cells.Item(1, 1).Formula="Hello, wps加载项!" + curSheet.Cells.Item(1, 1).Formula
+
+    }
+}
--- js/taskpane.js
+++ js/taskpane.js
@@ -30,9 +30,19 @@ function onbuttonclick(idStr)
                 break
         }
         case "addString":{
+            var str ='[{ "name": "cxh", "sex": "man"},{"name":"cxh1","sex":"man1"},{"name":"cxh2","sex":"man2"}]';
+            var obj = JSON.parse(str);
+            var arr = Object.keys(obj);
+            var keys = Object.keys(obj[0]);
             let curSheet = wps.EtApplication().ActiveSheet;
             if (curSheet){
-                curSheet.Cells.Item(1, 1).Formula="Hello, wps加载项!" + curSheet.Cells.Item(1, 1).Formula
+                for(let i = 1; i <= keys.length; i++) {
+                    curSheet.Cells.Item(1, i).Formula = keys[i-1]
+                    for (let j = 1; j <= arr.length; j++) {
+                        curSheet.Cells.Item(1 + j, i).Formula = obj[j - 1][keys[i-1]]
+                    }
+                    // curSheet.Cells.Item(1, 1).Formula="Hello, wps加载项!" + curSheet.Cells.Item(1, 1).Formula
+                }
             }
             break;
         }
@@ -46,6 +56,13 @@ function onbuttonclick(idStr)
                 textValue = textValue + doc.Name
                 document.getElementById("text_p").innerHTML = textValue
                 break
+        }
+        case "clear":{
+            let curSheet = wps.EtApplication().ActiveSheet;
+            if (curSheet){
+               curSheet.Cells.Clear();
+            }
+            break
         }
     }
 }
